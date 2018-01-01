42434278aaf8be898aae941a69393eb25653bdce
RohitAtmecs <rohit.suryawanshi@atmecs.com>
2018-01-01 18:00:24 +0530

    Update pipelineScript.groovy

--- pipelineScript.groovy
+++ pipelineScript.groovy
@@ -1,9 +1,6 @@
-node {
-   ws('C:\\apache-jmeter-3.3\\apache-jmeter-3.3\\extras') {
-       stage 'Run JMeter Test with Apache Ant'
-       def antHome = tool 'ant'
-       bat "pushd C:\\apache-jmeter-3.3\\apache-jmeter-3.3\\extras && ${antHome}\\bin\\ant -f build.xml"
-       step ([$class: 'ArtifactArchiver', artifacts: 'Test.html', fingerprint: true])
-   }
+ node {
+  stage 'Run JMeter Test'
+  bat 'C:/apache-jmeter-3.3/apache-jmeter-3.3/bin/jmeter.bat -n -t D:/PERFORMANCE/WoltersKluwer/RedirectRequest.jmx -Jusers=250 -l RedirectRequest.jtl'
+  step([$class: 'ArtifactArchiver', artifacts: '**/*.jtl, **/jmeter.log'])
 }
 
