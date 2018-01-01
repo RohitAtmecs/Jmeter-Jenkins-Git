103e909876010d7955fec2affa4e85d77d83c35e
RohitAtmecs <rohit.suryawanshi@atmecs.com>
2018-01-01 16:10:13 +0530

    Update pipelineScript.groovy

--- pipelineScript.groovy
+++ pipelineScript.groovy
@@ -1,5 +1,6 @@
 node {
   stage 'Run JMeter Test'
   bat 'C:/apache-jmeter-3.3/apache-jmeter-3.3/bin/jmeter.bat -n -t D:/PERFORMANCE/WoltersKluwer/RedirectRequest.jmx -Jusers=250 -l RedirectRequest.jtl'
+  step([$class: 'ArtifactArchiver', artifacts: '**/*.jtl, **/jmeter.log'])
 }
 
